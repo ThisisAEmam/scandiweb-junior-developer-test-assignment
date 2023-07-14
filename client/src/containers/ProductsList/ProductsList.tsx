9c7519a9f3d43d61302be92fbe03727d122bc21b
thisisaemam <abdelrahman.m.emam@gmail.com>
2023-07-15 01:48:47 +0300

    client: fixing some issues with ProductList

--- client/src/containers/ProductsList/ProductsList.tsx
+++ client/src/containers/ProductsList/ProductsList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsList.scss";
 import ProductCard from "../../components/ProductCard/ProductCard";
 
@@ -8,11 +8,19 @@ type Props = {
 };
 
 const ProductsList: React.FC<Props> = (props: Props) => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    if (props.products && Array.isArray(props.products)) {
+      setProducts(props.products);
+    }
+  }, [props.products]);
+
   return (
     <section className="products">
-      {props.products.length !== 0 ? (
+      {products.length !== 0 ? (
         <div className="products-list">
-          {props.products.map((item: ProductType) => (
+          {products.map((item: ProductType) => (
             <ProductCard key={item.sku} handleCheck={props.onSelectionCheck} {...item} />
           ))}
         </div>
